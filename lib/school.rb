a17cbf12813a4b5f50ed679621637b6fbe277ef0
Akim Clerge <akimclerge@gmail.com>
2019-11-07 05:47:31 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,8 +1,12 @@
 # code here!
 class School
-  def initialization
+  def initialization(roster)
+    @roster=roster
+  end
   roster=>{}
 end 
 
 school = School.new("Bayside High School")
+
+school.add_student("Zach Morris", 9)
 school.roster
